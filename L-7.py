830ffeafa81117c7005a8e418146eaa6ffedc755
teju-0505 <tejashvipanchal05@gmail.com>
2025-06-11 10:34:02 +0530

    Add files via upload

---
+++ L-7.py
@@ -0,0 +1,34 @@
+# Break Statement in python 
+
+# for i in range(1 , 6):
+#     if i == 4:
+#       break
+#     print("For Loop : " , i)
+
+# continue statement in python
+
+# for i in range(1 , 6):
+#     if i == 4:
+#        continue
+#     print("for loop : " , i)
+
+# for i in range(1 , 4):
+#     for j in range(1 , 2):
+#         if i == 2:
+#             break
+#     print("j loop: ", j)
+# print("i loop: ", i)
+
+# for i in range(1 , 4):              
+#     print("i loop: " , i)
+#     for j in range(2):
+#         if i == 2:
+#             break
+#         print("j loop: " , j)
+
+# pattern print in python
+
+# for i in range(1 , 7):
+#     for j in range(1 , i):
+#         print(f"{j}" , end="")
+#     print()
