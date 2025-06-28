5d52834c699db70428b124916248353418f03d20
teju-0505 <tejashvipanchal05@gmail.com>
2025-06-28 10:54:48 +0530

    Add files via upload

---
+++ L-9.py
@@ -0,0 +1,94 @@
+# list
+# tuple
+# set
+# dictionary
+
+# list 
+# mutable
+
+# my_list = [1, 2 , 3]
+
+# my_list[2] = 10     
+
+# print(my_list)
+
+# my_list = [1, 2 , 3]
+
+# my_list.append(20)
+
+# print(my_list)
+
+# my_list = [1, 2 , 3]
+
+# del my_list[0]
+
+# print(my_list)
+
+# tuple
+
+# my_tuple = [1 , 2 , 3]
+
+# my_tuple.append(4)
+
+# print(my_tuple)
+
+# my_tuple = [1 , 2 , 3]
+
+# del my_tuple[0]
+
+# print(my_tuple)
+
+# set
+
+# my_set = {1 , 2 , 3}
+
+# my_set.add(10)
+
+# print(my_set)
+
+# dictionary
+
+my_dics = {"1":"alice" , "2":"vishakha"}
+
+del my_dics["2"]
+
+print(my_dics)
+
+# name1 = "taxil"
+# name2 = "manan"
+# name3 = "harshil"
+# name4 = "prit"
+# name5 = "flagun"
+
+# print(name1)
+# print(name2)
+# print(name3)
+# print(name4)
+# print(name5)
+
+name_list = ["taxil" , "manan" , "harshil" , "prit" , "flagun"]
+
+print(name_list[0])
+print(name_list[1])
+print(name_list[2])
+print(name_list[3])
+print(name_list[4])
+
+# for user in name_list:
+# print(user)
+
+# Type casting constructor
+
+a = [1 , 2 ,3]
+
+b = tuple(a)
+
+c = set(b)
+
+d = dict(c)
+
+print(b)
+
+print(c)
+
+# print(d)
